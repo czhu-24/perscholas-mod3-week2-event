f991f7a5efdb3048940ff1498afac5d0fd0e4363
czhu-24 <blue.ink.stains@gmail.com>
2023-10-20 13:56:49 -0700

    gosh darn it

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const eventSchema = new mongoose.Schema({
+const eventSchema = new mongoose.Schema(
 	{
 		title: {type: String, required: true},
 		date: {type: Date, required: true},
@@ -16,3 +16,10 @@ const eventSchema = new mongoose.Schema({
 	}
 
 );
+
+// "Event" points to the events collection. it'll create a new events collection if it doesn't already exist
+
+// Event var is capitalized because we're exporting it 
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;

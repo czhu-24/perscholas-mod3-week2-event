83d79b299984b2400fe978283844fde4a16433b8
czhu-24 <blue.ink.stains@gmail.com>
2023-10-20 13:53:32 -0700

    sigh

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose");
 
-const eventSchema = new mongoose.Schema();
+const eventSchema = new mongoose.Schema({
+	{
+		title: {type: String, required: true},
+		date: {type: Date, required: true},
+		location: {type: String, required: true},
+		description: {type: String, required: true},
+		organizer: {
+			name: {type: String, required: true},
+			role: {type: String, required: true}
+		}
+	},
+	{
+		timestamps: true
+	}
+
+);
